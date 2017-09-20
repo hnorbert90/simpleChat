8baea9b04e9a490855d853a2015fa09a3fa7a5a1
hnorbert90 <hnorbert90@gmail.com>
2017-09-20 18:42:02 +0200

    init commit

---
+++ src/test/java/com/simpleChat/Chat/ChatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.simpleChat.Chat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
